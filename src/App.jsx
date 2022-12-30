d48a2ba75ff1636186f521af559ffb1a8943033b
igutsuyo <igutsuyo@gmail.com>
2022-12-30 16:45:01 +0900

    end

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import BgAnimation from './components/BgAnimation'
+import BottomGrid from './components/BottomGrid'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+
+function App() {
+  return <div className='h-screen overflow-hidden relative'>
+    <BgAnimation />
+    <Navbar />
+    <BottomGrid />
+    <Hero />
+  </div>
+}
+
+export default App
